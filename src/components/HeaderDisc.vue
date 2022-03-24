468a9c67d16b7528485be3d4bf38f5cbb6c16ce1
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-24 17:38:08 +0100

    problemi con scss

---
+++ src/components/HeaderDisc.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <div class="container">
+          <img src="../assets/logo.png" alt="">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'HeaderDisc'
+
+}
+</script>
+
+<style scoped lang='scss'>
+
+@import url(../assets/styles/variables.scss);
+    .container{
+        width: 1400px;
+        margin: 0 auto;
+        height: 100px;
+        background-color: $header-background;
+    }
+
+</style>
