d6aa5453835d5dab7875f60de65ebac6ff8cb33d
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-25 16:26:21 +0100

    bloccata a ieri

--- src/components/MainDisc.vue
+++ src/components/MainDisc.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
+  <main class="bg-primary">
+    <div class="container py-5">
+      <div class="row justify-content-center row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6">
+        <div v-if="cardRay == null" class="text-center">Loading Data</div>
+        <SingleCard v-for="card in cardRay" :key="card.id" :card-data="card"/>
+      </div>
+    </div>
 
-  </div>
+  </main>
 </template>
 
 <script>
+import axios from 'axios';
+import SingleCard from './SingleCard.vue'
 export default {
-
+  name: 'MainDisc',
+  components:{
+    SingleCard
+  },
+  data() {
+    return {
+      cardRay: null,
+    };
+  },
+  getApi() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        this.cardRay = response.data.response;
+        console.log(response.data.response)
+      });
+  },
 }
 </script>
 
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="card-disco bg-secondary m-3 p-4">
+    <img class="img-fluid" :src="cardData.poster" :alt="cardData.title">
+    <h3 class="text-center py-2">{{ cardData.title }}</h3>
+    <h4 class="text-center">{{ cardData.author }}</h4>
+    <h4 class="text-center">{{ cardData.year }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleCard',
+    props: {
+    cardData: Object,
+    },
+}
+
+</script>
+
+<style>
+
+</style>
